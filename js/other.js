531032bd3e46ccabb12d3c82c648d4e55befb2c2
shotaro <2b.sytr202@gmail.com>
2024-04-02 17:01:16 +0900

    other画像修正完了

--- js/other.js
+++ js/other.js
@@ -30,34 +30,6 @@ window.addEventListener('scroll', scroll_effect); // スクロール時に実行
 
 
 // DIY画像の切り替え
-const pushBtn = (btn, src) => {
-  document.getElementById(btn).onclick = function () {
-    document.getElementById("pics").src = src;
-
-    const contentsDiv = document.querySelectorAll('#change-image-navigation div');
-    contentsDiv.forEach(content => content.style.display = 'none');
-
-    document.getElementById("pic-" + btn).style.display = 'block';
-
-  }
-}
-
-pushBtn("diyBtn01", "picture/diy/table1.jpg")
-pushBtn("diyBtn02", "picture/diy/table2.jpg")
-pushBtn("diyBtn03", "picture/diy/bed1.jpg")
-pushBtn("diyBtn04", "picture/diy/bed2.jpg")
-pushBtn("diyBtn05", "picture/diy/fun1.jpg")
-pushBtn("diyBtn06", "picture/diy/step1.jpg")
-pushBtn("diyBtn07", "picture/diy/glass1.jpg")
-pushBtn("diyBtn08", "picture/diy/hanger1.jpg")
-pushBtn("diyBtn09", "picture/diy/light1.jpg")
-pushBtn("diyBtn10", "picture/diy/light2.jpg")
-pushBtn("diyBtn11", "picture/diy/chair1.jpg")
-pushBtn("diyBtn12", "picture/diy/chair2.jpg")
-
-
-
-
 var btn = document.getElementsByClassName('listItem');
 
 // ボタンの個数分ループ
